0863088fc903fa0b59f06d1f099cc76d76ab0840
Minizi <64017307+leemimi@users.noreply.github.com>
2023-11-21 22:14:01 +0900

    Title: 만나는 그 순간, Time: 157ms, Memory: 40MB, Status: Passed - CodeTree

--- 231121/만나는 그 순간/the-moment-we-meet.py
+++ 231121/만나는 그 순간/the-moment-we-meet.py
@@ -4,13 +4,13 @@ pos_a, pos_b = [0] * (MAX_T + 1), [0] * (MAX_T + 1)
 timeA=1
 for _ in range(n):
     d, t = tuple(input().split())
-    for _ in range(t):
+    for _ in range(int(t)):
         pos_a[timeA] = pos_a[timeA-1] + (1 if d=='R' else -1)
         timeA+=1
 timeB = 1
 for _ in range(m):
     d, t = tuple(input().split())
-    for _ in range(t):
+    for _ in range(int(t)):
         pos_b[timeB] = pos_b[timeB-1] + (1 if d=='R' else -1)
         timeB +=1
 
