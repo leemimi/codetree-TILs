3dc87af17a71af901f2bff79344c1b1a06bd3aff
Minizi <64017307+leemimi@users.noreply.github.com>
2023-11-21 23:18:02 +0900

    Title: 선두를 지켜라, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231121/선두를 지켜라/keep-the-lead.py
@@ -0,0 +1,28 @@
+n,m = map(int, input().split())
+posA = [0]*10001
+posB = [0]*10001
+time = 1
+for _ in range(n):
+    v,t = map(int, input().split())
+    for i in range(t):
+        posA[time] = posA[time-1] + v
+        time+=1
+
+time1 = 1
+for _ in range(m):
+    v,t = map(int, input().split())
+    for i in range(t):
+        posB[time1] = posB[time1-1]+v
+        time1 +=1
+
+leader, ans =0,0
+for i in range(1,time):
+    if posA[i] > posB[i]:
+        if leader == 2:
+            ans+=1
+        leader = 1
+    elif posA[i] < posB[i]:
+        if leader == 1:
+            ans+=1
+        leader = 2
+print(ans)
