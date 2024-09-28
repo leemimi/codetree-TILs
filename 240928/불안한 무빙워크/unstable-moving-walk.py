37b99994a9ec360664d547b9f64c3f0708cbf946
Minizi <64017307+leemimi@users.noreply.github.com>
2024-09-28 18:25:27 +0900

    Title: 불안한 무빙워크, Time: 137ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240928/불안한 무빙워크/unstable-moving-walk.py
+++ 240928/불안한 무빙워크/unstable-moving-walk.py
@@ -10,22 +10,23 @@ answer = 0
 while value < k:
     q.rotate(1)
     p.rotate(1)
+    if p[n-1] > 0:
+        p[n-1] = 0
 
-    for i in range(n-2, -1, -1):
-        if p[i] >0 and p[i+1] ==0 and q[i+1]>0:
-            p[i+1] = p[i]
-            p[i]= 0
-            q[i+1] -=1
-            if q[i+1] <=0:
+    for i in range(n-1, 0, -1):
+        if p[i] == 0 and p[i-1] > 0 and q[i-1]>0:
+            p[i] = 1
+            p[i-1]=0
+            q[i] -=1
+            if q[i] ==0:
                 value +=1
-
-    if p[n-1] >0:
+    if p[n-1] > 0:
         p[n-1] = 0
 
     if q[0] > 0 and p[0] == 0:
         p[0] = 1
         q[0] -=1
-        if q[0] <=0:
+        if q[0] ==0:
             value+=1
     answer+=1
 
