92a7b50eb835786eff53453169eae1a92e740350
Minizi <64017307+leemimi@users.noreply.github.com>
2024-09-28 18:17:11 +0900

    Title: 불안한 무빙워크, Time: 143ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240928/불안한 무빙워크/unstable-moving-walk.py
@@ -0,0 +1,36 @@
+from collections import deque
+n, k = map(int, input().split())
+arr = list(map(int, input().split()))
+people = [0]*len(arr)
+q = deque(arr)
+p = deque(people)
+value = 0
+answer = 0
+
+
+now = 0
+while value < k:
+    q.rotate(1)
+    p.rotate(1)
+
+    if people[n-1] >0:
+        people[n-1] = 0
+
+    for i in range(n-2, -1, -1):
+        if p[i] >0 and p[i+1] ==0 and q[i+1]>0:
+            p[i+1] = p[i]
+            p[i]= 0
+            q[i+1] -=1
+            if q[i+1] <=0:
+                q[i+1] = 0
+                value +=1
+
+    if q[0] > 0 and people[0] == 0:
+        p[0] +=1
+        q[0] -=1
+        if q[0] <=0:
+            q[0] = 0
+            value+=1
+    answer+=1
+
+print(answer)
